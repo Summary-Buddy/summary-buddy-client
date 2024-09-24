54947847b9b268ac256810bab4dbb182fb035057
skdod2453 <skdod2453@gmail.com>
2024-09-24 13:23:49 +0900

    login/register

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Login from './pages/Login'
 import Header from './components/Header';
+import Register from './pages/Register';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Routes>
         <Route path = "/" element = { <Login />} />
+        <Route path = "/Register" element = {<Register />} />
       </Routes>
     </>
   );
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,18 +1,51 @@
-import React from 'react'
+import React from 'react';
 import '../background.scss';
 
 export default function Login() {
   return (
     <div className='app-container'>
-      <div class="card" style={{width: "18rem"}}>
-        <div class="card-header">
-          Featured
+      <div className="card w-20 text-center" style={{ 
+          backgroundColor: "#FFF3C7", 
+          height: '40rem',
+          width: '35rem', 
+          border: '2px solid #FC819E', 
+          borderRadius: '15px' 
+        }}>
+        <div className="card-body d-flex flex-column align-items-center justify-content-center" style={{ height: '100%' }}>
+          <h5 className="card-title fw-bold" style={{ 
+              color: "#FC819E", 
+              fontSize: '30px', 
+              margin: '50px 0 30px' 
+            }}>
+            로그인
+          </h5>
+          
+          <div className="mb-3 w-75">
+            <label htmlFor="username" className="form-label"></label>
+            <input type="text" className="form-control" style={{height:'5rem', borderRadius: '15px', fontSize: '20px' }} id="username" placeholder="아이디를 입력하세요." />
+          </div>
+
+          <div className="mb-3 w-75">
+            <label htmlFor="password" className="form-label"></label>
+            <input type="password" className="form-control" style={{height:'5rem', borderRadius: '15px', fontSize: '20px' }} id="password" placeholder="비밀번호를 입력하세요." />
+          </div>
+
+          <a href="/Home" className="btn btn-primary fw-bold mb-3  d-flex  align-items-center justify-content-center" style={{ 
+              color: 'white', 
+              fontSize: '20px',
+              backgroundColor: '#FC819E', 
+              width: '20rem',
+              height: '4rem',
+              marginTop: '50px',
+              border: '#FC819E'
+            }}>
+            로그인하기
+          </a>
+
+          <p>
+            <a className="nav-link fw-bold" href="/MyPage">비밀번호 찾기</a>
+          </p>
         </div>
-        <ul class="list-group list-group-flush">
-          <li class="list-group-item">An item</li>
-          <li class="list-group-item">A second item</li>
-          <li class="list-group-item">A third item</li>
-        </ul>
       </div>
     </div>
   );
